a5acee570d203e2f0bff466eec70212469c1f7a4
Léo LETENEUR <leo@Leos-MBP.lan>
2026-03-05 10:46:19 +0100

    test

--- src/setupTests.tsx
+++ src/setupTests.tsx
@@ -1,7 +1,1 @@
-import '@testing-library/jest-dom';
-import { render, screen } from "@testing-library/react"
-import App from "./App" 
-test("renders text", () => {
-  render(<App />)
-  expect(screen.getByText(/hello/i)).toBeInTheDocument()
-})
+import "@testing-library/jest-dom";
